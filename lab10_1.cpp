e3e799ae8188902a5966b154ecd334c742e370d3
FrienDDDD <friendjunk@gmail.com>
2021-12-13 01:05:04 +0700

    Lab10_1.cpp complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,31 +3,59 @@
 using namespace std;
 
 int main(){	
+	
+	double loan,interest,payment ;
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
+	cout << setw(13) << left << "EndOfYear#" ; 
+	cout << setw(13) << left << "PrevBalance" ; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
+	cout << setw(13) << left << "NewBalance"  ;
 	cout << "\n";
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	int i = 1,years ;
+	double total,Interest,NewBalance,PrevBalance1 = loan,x = 0 ;
+	while(loan > 0){
+		years = i++ ;
+		if( years >= 2){
+			cout << setw(13) << left << years  ; 
+			cout << setw(13) << left << loan   ; 
+		}
+		Interest = loan*(interest/100.0) ;
+		total = loan+Interest ;
+		loan = loan+Interest - payment  ;
+		NewBalance = loan ;	
+		if(years == 1){
+			cout << setw(13) << left << years  ;
+			cout <<  setw(13) << left << PrevBalance1 ;
+		}
+		cout << setw(13) << left << Interest ;
+		cout << setw(13) << left << total ;
+		if(total < payment){
+			cout << setw(13) << left << total ; 
+		}else{
+			cout << setw(13) << left << payment ;
+		}	
+		if(NewBalance < 0){
+			cout << setw(13) << left << x << endl ;
+		}else{
+			cout << setw(13) << left << NewBalance << endl ;
+		}
+	}	
+
 	return 0;
 }
